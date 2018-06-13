571fd22cb0e43f1bbfa1399de64b6afb97ea6ed4
shant <shantv@gmail.com>
2018-06-13 19:18:40 +0100

    update

--- src/main/java/run/RTEAuthenticationEntryPoint.java
+++ src/main/java/run/RTEAuthenticationEntryPoint.java
@@ -24,7 +24,7 @@ public class RTEAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		setRealmName("Baeldung");
+		setRealmName("RunTheEarth");
 		super.afterPropertiesSet();
 	}
 }
